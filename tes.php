dfd6803c2b32b1aacde6a858e92be151b290c6a5
rezafauzan91 <reza.fauzan1991@gmail.com>
2015-08-14 19:21:31 +0700

    ngetes

--- tes.php
+++ tes.php
@@ -9,4 +9,5 @@ $months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
 $days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
 
 printf("%d years, %d months, %d days\n", $years, $months, $days);
+echo "tes2";
 ?>
